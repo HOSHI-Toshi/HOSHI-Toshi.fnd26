c66fb2d2c09afcc2658179435b6404c64017f216
HOSHI-Toshi <163362731+HOSHI-Toshi@users.noreply.github.com>
2024-04-21 13:21:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,111 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-// ボタン押された後の処理（回答者用）
-const ratingAllArray = []; // 全員の回答をまとめる配列宣言
-const fixed = document.getElementById("fixed"); // htmlからid"fixed"のHtml Element（送信ボタンのあるところ）を変数fixedに代入
-fixed.addEventListener("click",function() {
-  if (document.getElementById("name").value === "") { // 入力チェック（評価者）
-    window.alert("エラー：評価者が未入力です");
-  } else if (document.getElementById("ratingBdyCntrl1").value === ""
-      || document.getElementById("ratingBuru1").value === ""
-      || document.getElementById("ratingBiri1").value === "") {  // 入力チェック（case1の採点）
+// 関数）評価入力チェックと後処理
+/**
+ * @param {Array} raitingArray ケースごとの評価結果
+ * @returns {Boolean} 採点入力チェック結果（False：未入力あり）  
+ */
+function fixedRaiting(raitingArray) {
+  const inputErrChkBoolean = 
+    // case1
+    raitingArray[0] === "" || raitingArray[1] === "" || raitingArray[2] === ""
+    // case2
+    || raitingArray[3] === "" || raitingArray[4] === "" || raitingArray[5] === "" ;
+    // case3
+    // ...
+  if (inputErrChkBoolean) {  // 入力チェック（採点）
     window.alert("エラー：採点漏れがあります");
+    return false;
   } else if (window.confirm("評価確定してもよいですか？")) {   //入力最終チェック
     const ratingIndividualObject = {
       name:document.getElementById("name").value,
-      bdyCtrl:Number(document.getElementById("ratingBdyCntrl1").value),
-      buru:Number(document.getElementById("ratingBuru1").value),
-      biri:Number(document.getElementById("ratingBiri1").value)
+      // case1
+      bdyCtrl1:Number(raitingArray[0]),
+      buru1:Number(raitingArray[1]),
+      biri1:Number(raitingArray[2])
+      // case2
+      , bdyCtrl2:Number(raitingArray[3]),
+      buru2:Number(raitingArray[4]),
+      biri2:Number(raitingArray[5])
+      // case3
+      // ...
     }; // 回答をオブジェクト化
     ratingAllArray.push(ratingIndividualObject); // オブジェクト化した回答を配列の最後に追加
-    // ファイル出力 //
-    let raitingIndividualStrings
-      = document.getElementById("name").value + ","
-      + document.getElementById("ratingBdyCntrl1").value + ","
-      + document.getElementById("ratingBuru1").value + ","
-      + document.getElementById("ratingBiri1").value; // 0-a.回答（オブジェクト）を1つの文字列に変換
-    let outputFileName = `answer_${document.getElementById("name").value}.txt`; // 0-b.出力ファイル名に回答者名を差込 
-    const blob = new Blob([raitingIndividualStrings],{type:"text/plain"}); // 1. Blobオブジェクトを作成する
-    const link = document.createElement('a'); // 2. HTMLのa要素を生成
-    link.href = URL.createObjectURL(blob); // 3. BlobオブジェクトをURLに変換
-    link.download = outputFileName; // 4. ファイル名を指定する
-    link.click(); // 5. a要素をクリックする処理を行う
-    // ファイル出力ここまで //
-    // 画面テキストボックスの初期化
-    document.getElementById("name").value = "";
-    document.getElementById("ratingBdyCntrl1").value = "";
-    document.getElementById("ratingBuru1").value = "";
-    document.getElementById("ratingBiri1").value = "";
+    console.log(ratingAllArray);
+    return true;
   }
-  console.log(ratingAllArray);
+}
+
+// 評価ケース数をここで登録（定義）すること！（.htmlと一致させる）
+const numOfCase = 2;
+
+// 「評価送信」ボタンが押された後の処理
+const ratingAllArray = []; // 全員の回答をまとめる配列宣言
+const fixed = document.getElementById("fixed"); // htmlからid"fixed"のHtml Element（送信ボタンのあるところ）を変数fixedに代入
+
+fixed.addEventListener("click",function() {
+  const raitingCase1Array = [
+    // case1
+    document.getElementById("ratingBdyCntrl1").value,
+    document.getElementById("ratingBuru1").value,
+    document.getElementById("ratingBiri1").value
+    // case2
+    , document.getElementById("ratingBdyCntrl2").value,
+    document.getElementById("ratingBuru2").value,
+    document.getElementById("ratingBiri2").value
+    // case3
+    // , document.getElementById("ratingBdyCntrl3").value,
+    // document.getElementById("ratingBuru3").value,
+    // document.getElementById("ratingBiri3").value
+  ];
+  if (document.getElementById("name").value !== "") { // 入力チェック（評価者）
+    const existingRaitingBoolean = fixedRaiting(raitingCase1Array);  // 評価データ登録関数を呼び出す
+    // ファイル出力 //
+    if (existingRaitingBoolean) {
+      let indivdualAnserObj = ratingAllArray[ratingAllArray.length-1];
+      let raitingIndividualStrings = "";
+      for (const key in indivdualAnserObj) {
+        raitingIndividualStrings = raitingIndividualStrings + indivdualAnserObj[key] + ","
+      }  // 0-a.回答（オブジェクト）を1つの文字列に変換
+      let outputFileName = `answer_${document.getElementById("name").value}.txt`; // 0-b.出力ファイル名に回答者名を差込 
+      const blob = new Blob([raitingIndividualStrings],{type:"text/plain"}); // 1. Blobオブジェクトを作成する
+      const link = document.createElement('a'); // 2. HTMLのa要素を生成
+      link.href = URL.createObjectURL(blob); // 3. BlobオブジェクトをURLに変換
+      link.download = outputFileName; // 4. ファイル名を指定する
+      link.click(); // 5. a要素をクリックする処理を行う
+      // ファイル出力ここまで //
+      // 画面テキストボックスの初期化（名前）
+      document.getElementById("name").value = "";
+      // 画面テキストボックスの初期化（case1）
+      document.getElementById("ratingBdyCntrl1").value = "";
+      document.getElementById("ratingBuru1").value = "";
+      document.getElementById("ratingBiri1").value = "";
+      // 画面テキストボックスの初期化（case2）
+      document.getElementById("ratingBdyCntrl2").value = "";
+      document.getElementById("ratingBuru2").value = "";
+      document.getElementById("ratingBiri2").value = "";
+      // 画面テキストボックスの初期化（case3）
+      // ...
+    }
+  } else {
+    window.alert("エラー：評価者が未入力です");
+  }
 });
 
-// ボタン押された後の処理（管理者用）
+
+// 「管理者用」ボタンを押された後の処理
 const viewResult = document.getElementById("viewResult"); // Button部分のHTML要素の抽出
 viewResult.addEventListener("click",function() {
   let signStr = prompt("Input password")
   if (signStr === "password") {     // 管理者用かどうかパスワード認証
     let outputStrings = "name,Ans-A,Ans-B,Ans-C,<br>"; // 表示1行目のタイトル行の挿入
     for (const indivdualAnserObj of ratingAllArray) {
-      // console.log(indivdualAnserObj);
       for (const key in indivdualAnserObj) {
         outputStrings = outputStrings + `${indivdualAnserObj[key]},`
-        // console.log(outputStrings);
       }
       outputStrings = outputStrings + "<br>" // 配列に格納された全回答を文字列として行挿入。最後に改行も挿入。
     }
     document.getElementById("result").innerHTML = outputStrings;
   }
 });
-
-//// 次にやりたいこと ////
-// Case2、3の入力欄を増やす
